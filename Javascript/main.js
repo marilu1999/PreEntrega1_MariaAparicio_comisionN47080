fd920d7c1820095a3016613e0622335e81e11b0a
marialaura <marialauraparicio@gmail.com>
2023-10-02 21:15:28 -0300

    correccion

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,54 +1,51 @@
-alert("Bienvenido al generador de turnos para continuar haga click en aceptar")
-alert("La consulta Psicologia tiene una duración de 1 hora, para reservar su turno sera necesario completar el siguiente formulario y abonar la mitad del honario la consulta tiene un costo de:$4.000")
 
-let nombre = prompt("Ingrese Su Nombre ")
+alert("Bienvenido al generador de turnos. Haga clic en aceptar para continuar.");
 
-let apellido = prompt( "Ingrese Su Apellido ")
+let continuar = true;
 
-alert("Hola"+" "+ nombre + " "+apellido + " " +"a continuación te solicitaremos informacion adicional para generar el turno")
-
-let dia = prompt ("ingresa el dia de la semana de lunes a viernes en que deseas atenderte")
-
-let horario = prompt ("cuentanos en que turno te gustaria ser atendido: mañana o tarde")
-
- function generadorTurno (nombre,apellido,dia,turno){
+while (continuar) {
+  alert("La consulta de Psicología tiene una duración de 1 hora y un costo de $4,000.");
+  
+  let nombre = prompt("Ingrese su nombre:");
+  let apellido = prompt("Ingrese su apellido:");
+  
+  alert("Hola " + nombre + " " + apellido + ", a continuación te solicitaremos información adicional para generar el turno.");
+  
+  let dia = prompt("Ingresa el día de la semana en el que deseas atenderte (lunes a viernes):");
+  let horario = prompt("Cuéntanos en qué turno te gustaría ser atendido (mañana o tarde):");
+  
+  function generadorTurno(nombre, apellido, dia, turno) {
     switch (dia){
-        case "lunes":
-            return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
-            break;
-        case  "martes":
-            return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
-        case "miercoles":
-            return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
-        case " jueves":
-            return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
-        case "viernes":
-            return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
-        default:
-            if (dia !== "lunes" || dia !== "martes" || dia !== "miercoles" || dia !== "jueves" || dia !== "viernes"){
-                alert ("ingrese un dia valido")
-            dia = prompt("ingrese un dia valido ejemplo lunes")
-            break;}
-          
-
-    }
-    
-}   
- 
-generadorTurno(nombre,apellido,dia,horario);
- 
-alert ("Para continuar con la reserva debera ingresar el comprobante de pago")
-let comprobante = prompt("¿Envio el comprobante al email turnos@mipsicologoideal.com.ar? ")
-
-if (comprobante === "si"){
-    
-alert("Su turno ha sido agendado en el transcurso del dia se verificara y ante cualquier incoveniente nos estaremos comunicando Muchas gracias");
-}else{
-    alert( "Su turno ha sido reservado pero puede ser cancelado en cualquier momento por favor genere el comprobante y envielo al email: turnos@mipsicologoideal.com.ar para asegurar su turno Muchas gracias");
-}
-
-
- 
+                case "lunes":
+                    return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
+                    break;
+                case  "martes":
+                    return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
+                    break;
+                case "miercoles":
+                    return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
+                    break;
+                case " jueves":
+                    return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
+                    break;
+                case "viernes":
+                    return alert("Estimado"+" "+nombre+" "+ apellido+ " " +"su turno ha sido reservado para el dia"+" " + dia + " "+ "en el turno" + " "+ turno);
+                    break;
+                default:
+                    if (dia !== "lunes" || dia !== "martes" || dia !== "miercoles" || dia !== "jueves" || dia !== "viernes"){
+                        alert ("ingrese un dia valido")
+                    dia = prompt("ingrese un dia valido ejemplo lunes")
+                    break;}}}
+  generadorTurno(nombre, apellido, dia, horario);
+  
+  let comprobante = prompt("¿Enviarás el comprobante de pago al correo electrónico turnos@mipsicologoideal.com.ar? (sí/no)");
+  
+  if (comprobante == "si") {
+    alert("Su turno ha sido agendado. Se verificará el comprobante de pago. ¡Gracias!");
+  } else {
+    alert("Su turno ha sido reservado, pero puede ser cancelado en cualquier momento. Por favor, genere el comprobante y envíelo al correo electrónico turnos@mipsicologoideal.com.ar para asegurar su turno. ¡Gracias!");
+  } break;}
+  
 
 
 
